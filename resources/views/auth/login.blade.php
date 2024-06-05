072940fea1fe7ec9ac620824891bd435cf9c6a4a
Luis Poter <potercanoluis@gmail.com>
2024-06-05 18:21:19 -0500

    Se añadio archivo modify_user con los nuevos campos, y en login.blade se ha creado la vista de user y password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login</title>
 </head>
 <body>
-    <h1>Prueba</h1>
+    <form class="container">
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Contraseña</label>
+          <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+      </form>
 </body>
 </html>
